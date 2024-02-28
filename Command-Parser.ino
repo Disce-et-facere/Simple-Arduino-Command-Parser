fa606deb39ba95907a4b6a447a41beb0ba18d5ec
Robert Krantz <krantzr@hotmail.se>
2024-02-28 15:57:36 +0100

    Update Command-Parser.ino
    Added more control to the disco mode

--- Command-Parser.ino
+++ Command-Parser.ino
@@ -24,7 +24,7 @@ void setup() {
 
     Serial.println("Type HELP for avaliable Commands...");
 }
-// Function to display the input options
+
 void commandHelp(){
     Serial.println("Avaliable Commands:");
     Serial.println("GREEN LED ON - Switches the Green led mode to ON");
@@ -44,52 +44,42 @@ void turnOnLed(int color){
 
     switch(color){
         //Green
-        case 1:
-        {
-            if(greenLedState){
-                Serial.println("Green led is already ON!\n");
-                return;
-            }else{
+        case GREEN:
+        {   
+
                 digitalWrite(GREEN_LED_PIN, ON);
                 Serial.println("GREEN LED IS ON!\n");
                 greenLedState = ON;
-            }
+        
             return;
         }
-        case 2:
+        case RED:
         {
-            if(redLedState){
-                Serial.println("Red led is already ON!\n");
-                return;
-            }else{
+  
                 digitalWrite(RED_LED_PIN, ON);
                 Serial.println("RED LED IS ON!\n");
                 redLedState = ON;
-            }
+            
             return;
         }
-        case 3:
+        case YELLOW:
         {
-            if(yellowLedState){
-                Serial.println("Yellow led is already ON!\n");
-                return;
-            }else{
+    
                 digitalWrite(YELLOW_LED_PIN, ON);
                 Serial.println("YELLOW LED IS ON!\n");
                 yellowLedState = ON;
-            }
+            
             return;
         }
     }
-
 }
 
-// Function to turn off 1 of 3 leds depending of choosen color
+// Function to turn off led 1 of 3 leds depending of choosen color
 void turnOffLed(int color){
 
         switch(color){
         //Green
-        case 1:
+        case GREEN:
         {
             if(!greenLedState){
                 Serial.println("Green led is already OFF!\n");
@@ -101,7 +91,7 @@ void turnOffLed(int color){
             }
             return;
         }
-        case 2:
+        case RED:
         {
             if(!redLedState){
                 Serial.println("Red led is already OFF!\n");
@@ -113,7 +103,7 @@ void turnOffLed(int color){
             }
             return;
         }
-        case 3:
+        case YELLOW:
         {
             if(!yellowLedState){
                 Serial.println("Yellow led is already OFF!\n");
@@ -128,25 +118,36 @@ void turnOffLed(int color){
     }
 }
 
-// Function to start the DiscoMode
+// Function to turn DiscoMode to either on or off
 void discoMode(){
 
-    digitalWrite(GREEN_LED_PIN, ON);
+    if(greenLedState){
+      digitalWrite(GREEN_LED_PIN, ON);
+    }
     delay(100);
+    if(redLedState){
     digitalWrite(RED_LED_PIN, ON);
+    }
+    if(greenLedState){
     digitalWrite(GREEN_LED_PIN, OFF);
+    }
     delay(100);
+    if(yellowLedState){
     digitalWrite(YELLOW_LED_PIN, ON);
+    }
+    if(redLedState){
     digitalWrite(RED_LED_PIN, OFF);
+    }
     delay(100);
+    if(yellowLedState){
     digitalWrite(YELLOW_LED_PIN, OFF);
-
+    }
     return;
 }
 
 void loop() {
 
-    if (Serial.available() > 0) { //Command Parser with options -->
+    if (Serial.available() > 0) { //Command Parser -->
 
         String input = Serial.readStringUntil('\n'); 
 
@@ -155,8 +156,15 @@ void loop() {
             commandHelp();
 
         }else if(input == "GREEN LED ON" || input == "green led on"){
-
-            turnOnLed(GREEN);
+            
+            if(!discoState){
+              turnOnLed(GREEN);
+            }else if(greenLedState){
+              Serial.println("Green led is already ON!\n");
+            }else{
+              greenLedState = ON;
+            }
+            
 
         }else if(input == "GREEN LED OFF" || input == "green led off"){
 
@@ -164,7 +172,13 @@ void loop() {
 
         }else if(input == "RED LED ON" || input == "red led on"){
 
-            turnOnLed(RED);
+            if(!discoState){
+              turnOnLed(RED);
+            }else if(redLedState){
+              Serial.println("Red led is already ON!\n");
+            }else{
+              redLedState = ON;
+            }
 
         }else if(input == "RED LED OFF" || input == "red led off"){
 
@@ -172,16 +186,23 @@ void loop() {
 
         }else if(input == "YELLOW LED ON" || input == "yellow led on"){
 
-            turnOnLed(YELLOW);
+            if(!discoState){
+              turnOnLed(YELLOW);
+            }else if(yellowLedState){
+              Serial.println("Yellow led is already ON!\n"); 
+            }else{
+              yellowLedState = ON;
+            }
 
         }else if(input == "YELLOW LED OFF" || input == "yellow led off"){
             
             turnOffLed(YELLOW);
 
         }else if(input == "DISCO MODE ON" || input == "disco mode on"){
-            greenLedState = OFF;
-            redLedState = OFF;
-            yellowLedState = OFF;
+          
+            greenLedState = ON;
+            redLedState = ON;
+            yellowLedState = ON;
             discoState = ON;
             Serial.println("DISCO MODE ON!\n");
 
@@ -194,10 +215,13 @@ void loop() {
 
             Serial.println("Invalid input!\n");
 
-        }// <--- Command Parser with options
-    }
+        }
+    }// <--- Command Parser
+    
     // using the already existing loop for the disco mode
     if(discoState){
-        discoMode(); 
+
+        discoMode();
+        
     }
 }
